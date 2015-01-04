511b8a3c922ee40a2c5e553765ca750f13b34692
stevehe-campray <steve.he@campray.com>
2015-01-04 20:05:54 +0800

    update codes

--- MPOS/src/main/java/com/mpos/service/impl/MenuServiceImpl.java
+++ MPOS/src/main/java/com/mpos/service/impl/MenuServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import org.hibernate.Criteria;
 import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -92,8 +93,11 @@ public class MenuServiceImpl implements MenuService {
 	}
 
 
-	public List<Tmenu> getAllMenu() {		
-		return menuDao.LoadAll();
+	public List<Tmenu> getAllMenu() {
+		Criteria criteria=menuDao.createCriteria();
+		return criteria.add(Restrictions.eq("status",true))				
+				.list();		
+		//return menuDao.LoadAll();
 	}
 		
 }
