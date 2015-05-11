e4530f53123a093222844001ed7d6b3cd91ca66c
davepu-campray <dave.pu@campray.com>
2015-05-11 14:14:11 +0800

    fixed

--- MPOS/src/main/java/com/mpos/action/PaymentController.java
+++ MPOS/src/main/java/com/mpos/action/PaymentController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
@@ -15,7 +14,7 @@ import com.alibaba.fastjson.JSON;
 @RequestMapping(value = "payment")
 public class PaymentController extends BaseController {
 	
-	@RequestMapping(method=RequestMethod.GET)
+	@RequestMapping
 	@ResponseBody
 	public String test(HttpServletRequest request,String code){
 		System.out.println(request.getRemoteAddr());
@@ -24,9 +23,9 @@ public class PaymentController extends BaseController {
 			System.out.println(re.get(key));
 		}
 		Map<String, Object> res = getHashMap();
-		res.put("status", true);
-		res.put("code", code);
-		return JSON.toJSONString(res);
+		res.put("is_success", "T");
+		res.put("out_trade_no", System.currentTimeMillis());
+		return JSON.toJSONString("success");
 	}
 	
 }
